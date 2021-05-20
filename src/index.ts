c34f491b937a8fa2016dea3d83bf920c3708a0f7
John Garzon <gajohn511@gmail.com>
2021-05-20 11:45:11 -0400

    setup middlewear utility file part 1

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import apiEndpoint from "./api";
 import path from "path";
 import ejs from "ejs";
+import middlewear from "./utilities/middlewear";
 
 const app = express();
 const port = 3000;
@@ -21,3 +22,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`server started listening at http://localhost:${port}`);
 });
+
+export default app;
---
+++ src/utilities/middlewear.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const getListOfFiles = (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => {
+    //
+};
+
+const middlewear = {
+    getListOfFiles
+};
+
+export default middlewear;

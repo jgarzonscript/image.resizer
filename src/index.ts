0bb53744a2d3d43b212bbae836a1acfe7e78645f
John Garzon <gajohn511@gmail.com>
2021-05-19 22:50:52 -0400

    added ejs view engine

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express from "express";
 import apiEndpoint from "./api";
+import path from "path";
+import ejs from "ejs";
 
 const app = express();
 const port = 3000;
 
+app.engine(".html", ejs.renderFile);
+app.set("views", __dirname);
+
 app.use("/api", apiEndpoint);
+
+//GET
+app.get("/", (req, res) => {
+    res.render("index.html", {
+        mymessage: "hello world"
+    });
+});
 
 app.listen(port, () => {
     console.log(`server started listening at http://localhost:${port}`);

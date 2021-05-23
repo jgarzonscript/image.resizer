b2a373b38f6a79e32cefd5fc13cfb1f8b11268ae
John Garzon <gajohn511@gmail.com>
2021-05-23 00:09:33 -0400

    middlewear config - completed getListOfFiles

--- src/utilities/middlewear.ts
+++ src/utilities/middlewear.ts
@@ -3,55 +3,35 @@ import { BaseEncodingOptions, Dirent, PathLike } from "fs";
 import { readdir } from "fs/promises";
 // import path from "path";
 
-interface myread {
-    (
-        path: PathLike,
-        options?:
-            | (BaseEncodingOptions & { withFileTypes?: false })
-            | BufferEncoding
-            | null
-    ): Promise<string[]>;
-    (
-        path: PathLike,
-        options: { encoding: "buffer"; withFileTypes?: false } | "buffer"
-    ): Promise<Buffer[]>;
-    (
-        path: PathLike,
-        options?:
-            | (BaseEncodingOptions & { withFileTypes?: false })
-            | BufferEncoding
-            | null
-    ): Promise<string[] | Buffer[]>;
-    (
-        path: PathLike,
-        options: BaseEncodingOptions & { withFileTypes: true }
-    ): Promise<Dirent[]>;
-}
-
 const getAllFiles = async (
-    read: myread,
     dir = "./images",
     options: BaseEncodingOptions & { withFileTypes: true }
 ) => {
     try {
-        const result = await read(dir, options);
-        const test = true;
+        const files = (await readdir(dir, options))
+            .filter((item) => !item.isDirectory())
+            .map((file) => file.name);
+        return files;
     } catch (error) {
-        //
+        throw new Error(error);
     }
 };
 
-// const getFiles = async (files: Promise<Dirent[]>) => {
-//     const result = await files;
-//     const test = true;
-// };
-
-const getListOfFiles = (
+const getListOfFiles = async (
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
 ) => {
-    getAllFiles(readdir, undefined, { withFileTypes: true });
+    try {
+        res.locals.files = await getAllFiles("./images", {
+            withFileTypes: true
+        });
+        next();
+    } catch (error) {
+        console.log(error);
+        res.locals.error = error;
+        next();
+    }
 };
 
 const middlewear = {

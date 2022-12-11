42fe6d3580a8eb2c290551163b7e930c5874f876
John Garzon <gajohn511@gmail.com>
2022-12-11 14:09:34 -0500

    minor misc changes to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,26 @@ module.exports = {
         commonjs: true,
         node: true,
         browser: true,
-        es6: true,
-        jest: true
+        // es6: true,
+        // jest: true,
+        es2021: true
     },
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        ecmaVersion: 12,
-        sourceType: "module",
-        ecmaFeatures: {
-            modules: true
-        }
+        ecmaVersion: "latest"
+        // sourceType: "module",
+        // ecmaFeatures: {
+        //     modules: true
+        // }
         // tsconfigRootDir: __dirname,
         // project: ["./tsconfig.json"]
     },
     plugins: ["@typescript-eslint"],
     extends: [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
         // "plugin:@typescript-eslint/recommended-requiring-type-checking"
     ],
     rules: {
